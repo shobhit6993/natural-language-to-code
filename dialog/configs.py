670e62a34358618381a9403e3117f09694344985
Shobhit Chaurasia <sc.shobhit@gmail.com>
2017-03-08 20:15:08 -0600

    Change default parameters for dialog policy

--- dialog/configs.py
+++ dialog/configs.py
@@ -1,9 +1,9 @@
 class DialogConfiguration(object):
     # Threshold over which the slot is deemed as confidently-filled without a
     # need for confirmation.
-    alpha = 0.7
+    alpha = 0.75
     # Threshold over which -- and below `alpha` -- the slot confirmed explicitly
     # before being fixed.
-    beta = 0.4
+    beta = 0.25
 
     assert (alpha >= beta)
