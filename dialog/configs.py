952ed173bd968954ce76897a46238c8e0b6a6a88
Shobhit Chaurasia <sc.shobhit@gmail.com>
2017-03-15 17:17:50 -0500

    Change default dialog configurations

--- dialog/configs.py
+++ dialog/configs.py
@@ -1,7 +1,7 @@
 class DialogConfiguration(object):
     # Threshold over which the slot is deemed as confidently-filled without a
     # need for confirmation.
-    alpha = 0.75
+    alpha = 0.85
     # Threshold over which -- and below `alpha` -- the slot confirmed explicitly
     # before being fixed.
     beta = 0.25
