9c1aed2833abb9196be5a30444eea0088a4507d1
Shobhit Chaurasia <sc.shobhit@gmail.com>
2017-03-25 17:15:57 -0500

    Fix sys utterance analyzer to respect new format

--- log_analysis/sys_utterance_analyzer.py
+++ log_analysis/sys_utterance_analyzer.py
@@ -14,32 +14,33 @@ class SysUtteranceAnalyzer(object):
         self.label_map = label_map
 
     def extract_trigger_channel_from_ask_slot(self, utterance):
-        start = len("Which event on ")
+        start = len("Which event on the ")
         end = utterance.index(" should I")
         channel_name = utterance[start:end]
         return self.label_map.trigger_channel_from_name(channel_name)
 
     def extract_action_channel_from_ask_slot(self, utterance):
-        start = len("What should I do on ")
+        start = len("What should I do on the ")
         end = utterance.index(" every time")
         channel_name = utterance[start:end]
         return self.label_map.action_channel_from_name(channel_name)
 
     def extract_trigger_channel_from_confirm(self, utterance):
-        start = len("Do you want an event on ")
+        start = len("Do you want an event on the ")
         end = utterance.index(" service")
         channel_name = utterance[start:end]
         return self.label_map.trigger_channel_from_name(channel_name)
 
     def extract_action_channel_from_confirm(self, utterance):
-        start = len("Do you want to use ")
+        start = len("Do you want to use the ")
         end = utterance.index(" service")
         channel_name = utterance[start:end]
         return self.label_map.action_channel_from_name(channel_name)
 
     def extract_trigger_fn_from_confirm(self, utterance, channel):
         start = len("Do you want to trigger the applet ")
-        fn_desc = utterance[start:-1]
+        end = utterance.index("? (yes/no)")
+        fn_desc = utterance[start:end]
         return self.label_map.trigger_fn_from_description(fn_desc, channel)
 
     def extract_action_fn_from_confirm(self, utterance, channel):
